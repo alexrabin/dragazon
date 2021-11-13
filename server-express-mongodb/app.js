7fd80bf15f176114dcdc9edf367e9e8309736470
Lonnie Mitchell <lonniejmitchell@gmail.com>
2021-11-13 17:21:28 -0600

    Added product routes to App.js

--- server-express-mongodb/app.js
+++ server-express-mongodb/app.js
@@ -10,6 +10,7 @@ dotenv.config();
 var mongoose = require("mongoose");
 
 var usersRouter = require("./routes/users");
+var productsRouter = require("./routes/products");
 var tasksRouter = require("./routes/tasks");
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(cors());
 
 app.use("/tasks", tasksRouter);
 app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 //var mongoDB = "mongodb://127.0.0.1/database";
 var mongoDB =
