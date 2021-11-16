8b483e739393cd1a3c95ff5709b9d21999222b4a
Lonnie Mitchell <lonniejmitchell@gmail.com>
2021-11-15 22:28:18 -0600

    adjusted route cart.js code

--- server-express-mongodb/routes/cart.js
+++ server-express-mongodb/routes/cart.js
@@ -2,7 +2,7 @@ const Cart = require("../models/Cart");
 
 const router = require("express").Router();
 
-//CREATE//
+//CREATE NEW CART//
 router.post("/", async (req, res) => {
   const newCart = new Cart(req.body);
 
@@ -14,7 +14,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-//UPDATE//
+//UPDATE CART//
 router.put("/:id",  async (req, res) => {
   try {
     const updatedCart = await Cart.findByIdAndUpdate(
@@ -30,32 +30,11 @@ router.put("/:id",  async (req, res) => {
   }
 });
 
-//DELETE//
+//DELETE CART//
 router.delete("/:id", async (req, res) => {
   try {
     await Cart.findByIdAndDelete(req.params.id);
     res.status(200).json("Cart has been deleted...");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//GET USER CART//
-router.get("/find/:userId",  async (req, res) => {
-  try {
-    const cart = await Cart.findOne({ userId: req.params.userId });
-    res.status(200).json(cart);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
- //GET ALL//
-
-router.get("/", async (req, res) => {
-  try {
-    const carts = await Cart.find();
-    res.status(200).json(carts);
   } catch (err) {
     res.status(500).json(err);
   }
