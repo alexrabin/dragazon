1c9c256c50361d311f8080165f0030e5610df9e1
Lonnie Mitchell <lonniejmitchell@gmail.com>
2021-11-18 15:55:45 -0600

    remove sidenav drop box

--- client-react/src/components/NavigationBar.js
+++ client-react/src/components/NavigationBar.js
@@ -83,16 +83,7 @@ export default function NavigationBar() {
                     Logout
                   </Nav.Link>
                 )}
-                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
+               
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
