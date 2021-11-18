b2a9f144f3c3ad60ecf7b7e9e8f81dd81a52cb7b
Alex <alexrabin@me.com>
2021-11-17 18:17:41 -0700

    fixed form on create product

--- client-react/src/components/CreateProductModal.js
+++ client-react/src/components/CreateProductModal.js
@@ -16,55 +16,57 @@ export default function CreateProductModal(props) {
                 </Modal.Header>
                 <Modal.Body>
                     
-                    <Form>
+                    <Form onSubmit={(event) =>{
+                        event.preventDefault();
+
+                        props.onHide();
+                        var newProduct = {
+                            categories: categoriesRef.current.value,
+                            img: imageRef.current.value,
+                            desc: descRef.current.value,
+                            title: titleRef.current.value,
+                            inStock: inStock,
+                            price: priceRef.current.value
+                        }
+                        props.onCreateProduct(newProduct);
+                    }}>
                             <Form.Group className="mb-3">
                             <Form.Label>Image</Form.Label>
-                            <Form.Control ref={imageRef}/>
+                            <Form.Control ref={imageRef} required/>
                             </Form.Group>
                             <Form.Group className="mb-3">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control ref={titleRef} />
+                            <Form.Control ref={titleRef} required />
                             </Form.Group>
                             <Form.Group className="mb-3">
                             <Form.Label>Description</Form.Label>
-                            <Form.Control ref={descRef} />
+                            <Form.Control ref={descRef} required/>
                             </Form.Group>
                             <Form.Group className="mb-3">
-                            <Form.Label>Categories</Form.Label>
-                            <Form.Control ref={categoriesRef} />
+                            <Form.Label>Categories (Seperated by comma)</Form.Label>
+                            <Form.Control ref={categoriesRef} required />
                             </Form.Group>
                             <Form.Group className="mb-3">
                             <Form.Label>Price in cents</Form.Label>
-                            <Form.Control ref={priceRef} />
+                            <Form.Control ref={priceRef} required/>
                             </Form.Group>
                             <Form.Group className="mb-3">
-                            <Form.Check type="checkbox" label="In Stock" checked ={inStock} onChange={(e) => {
+                            <Form.Check type="checkbox" label="In Stock" required checked ={inStock} onChange={(e) => {
                                 setInStock(e.target.checked)
                             }} />
 
                             </Form.Group>
-
+                            
+                <Button type="submit" variant="primary" className={"m-2"}>
+                    Create
+                </Button>
+                <Button variant="secondary" onClick={props.onHide}>
+                                Cancel
+                            </Button>
                         </Form>           
                 </Modal.Body>
                 <Modal.Footer>
-                <Button variant="secondary" onClick={props.onHide}>
-                    Cancel
-                </Button>
-                <Button variant="primary" onClick={()=> {
-                    props.onHide();
-                    var newProduct = {
-                        categories: categoriesRef.current.value,
-                        img: imageRef.current.value,
-                        desc: descRef.current.value,
-                        title: titleRef.current.value,
-                        inStock: inStock,
-                        price: priceRef.current.value
-                    }
-                    props.onCreateProduct(newProduct);
-
-                }}>
-                    Create
-                </Button>
+                
                 </Modal.Footer>
             </Modal>
     )
